767f9a2c94e30c74e5d250bc083d2cf728b59342
Matheus Castiglioni <matheushcastiglioni@gmail.com>
2020-09-17 08:47:07 -0300

    Enviando para Github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import useSWR from 'swr'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fetcher = (...params) => axios
+  .get(...params)
+  .then(resp => resp.data)
+
+const useProjectInfo = () => {
+  const { data, error } = useSWR(
+    'https://api.github.com/repos/mahenrique94/jlib',
+    fetcher
+  )
+
+  return {
+    isLoading: !data && !error,
+    data,
+    error,
+  }
 }
 
-export default App;
+const Name = () => {
+  const { data } = useProjectInfo()
+  return <h1>Projeto: {data.name}</h1>
+}
+
+const Stars = () => {
+  const { data } = useProjectInfo()
+  return <h3>Estrelas: {data.stargazers_count}</h3>
+}
+
+const App = () => {
+  const {isLoading, error} = useProjectInfo()
+
+  if (isLoading) {
+    return <h1>Buscando dados do projeto...</h1>
+  }
+
+  if (error) {
+    return <h1>Deu erro: {error.message}</h1>
+  }
+  
+  return (
+    <>
+      <Name />
+      <Stars />
+    </>
+  )
+}
+
+export default App
